12858b3a85c6a7a236f911ee5738c8baf1fcf3a9
Nir-Chauser <chauser.nir@gmail.com>
2026-04-25 17:05:14 +0300

    Clean repository contents for submission

--- 123456789_123456789/Rentals_App/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('query_results/', views.query_results, name='query_results'),
-    path('add_rental/', views.add_rental, name='add_rental'),
-    path('search_analysis/', views.search_analysis, name='search_analysis'),
-]
